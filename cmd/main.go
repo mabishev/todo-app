e7e68d92d94b9c764fd7839ad2bb05c085a1294f
mabishev <7070366@gmail.com>
2025-11-09 10:08:52 +0500

    create srv on main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/mabishev/todo-app.git"
+)
+
+func main() {
+	srv := new(todo.Server)
+	if err := srv.Run("8080"); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
 }
